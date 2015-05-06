b582355e55ba92e65a46939dccb0917fd7bfd07c
remindjp <remind.jp@gmail.com>
2015-05-06 17:02:39 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,12 +5,12 @@ require_relative 'models'
 set :bind, '0.0.0.0'
 set :port, 8080
 # supposed to modify hello world.. where is this????
-get '/' do
-  "Hello World! Welcome to Las Vegas!\n"+
+# get '/' do
+#  "Hello World! Welcome to Las Vegas!\n"+
 # 
 #
-  "User is #{ENV['ADMIN_USERNAME']}\n"
-end
+#  "User is #{ENV['ADMIN_USERNAME']}\n"
+# end
 
 def configure_database
   if ENV['RACK_ENV']=="production"
