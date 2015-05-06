1344577dac939715b3d4e02f590dc3c3a5313f4e
remindjp <remind.jp@gmail.com>
2015-05-06 15:59:50 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require_relative 'models'
 set :bind, '0.0.0.0'
 set :port, 8080
 # supposed to modify hello world.. where is this????
-
+puts "Testing!"
 def configure_database
   if ENV['RACK_ENV']=="production"
     while !self.connect_to_database_prod
